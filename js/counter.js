26ce55d278757b2a5da2e4c089cd6ded6384691d
1451678 <1451678+github@gmail.com>
2019-02-04 15:32:07 +0100

    Remove fixed paths

--- js/counter.js
+++ js/counter.js
@@ -4,16 +4,16 @@ POP.counter = (function() {
   var countNumber = (function () {
     var countNumber = null;
     $.ajax({
-      'async': false,
-      'global': false,
-      'url': 'http://maniyar.de/pop-bubbles/counter.json',
-      'dataType': "json",
-      'success': function (data) {
+      async: false,
+      global: false,
+      url: "/counter/counter.json",
+      dataType: "json",
+      success: function (data) {
         countNumber = data.counter;
       }
     });
 	return countNumber;
   })();
-  $('.counter-numbers').html(countNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-  $.post("http://maniyar.de/pop-bubbles/counter.php", {ajax: true});
+  $(".counter-numbers").html(countNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+  $.post("/counter/counter.php", {ajax: true});
 });
